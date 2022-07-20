9aca826d30ee6b12a7bcbe168055036f400a0af4
yomoginna <yomoginna@gmail.com>
2022-07-20 11:21:30 +0900

    changed port num

--- main.py
+++ main.py
@@ -61,5 +61,6 @@ def handle_message(event):
 
 if __name__ == "__main__":
     # app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    #port = int(os.getenv("PORT"))
+    #app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port="8000")
